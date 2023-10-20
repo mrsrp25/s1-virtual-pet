e23b55600d4fa824044f97653fd384cf88ed5086
Your Name <you@example.com>
2023-10-20 13:40:01 -0700

    virtual pet

--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -3,10 +3,16 @@
  * @author Cam
  * @author ?
  */
+
+import java.awt.Component;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class VirtualPet {
     
     VirtualPetFace face;
-    int hunger = 0;   // how hungry the pet is.
+    int excitement = 0;   // how hungry the pet is.
     
     // constructor
     public VirtualPet() {
@@ -15,25 +21,40 @@ public class VirtualPet {
         face.setMessage("Hello.");
     }
     
-    public void feed() {
-        if (hunger > 10) {
-            hunger = hunger - 10;
+    public void play() {
+        if (excitement  > 10) {
+            excitement = excitement - 10;
         } else {
-            hunger = 0;
+            excitement = 0;
         }
-        face.setMessage("Yum, thanks");
-        face.setImage("normal");
+        face.setMessage("Im, excited");
+        face.setImage("joyful");
     }
     
-    public void exercise() {
-        hunger = hunger + 3;
-        face.setMessage("1, 2, 3, jump.  Whew.");
-        face.setImage("tired");
+    public void fight() {
+        excitement = excitement + 3;
+        face.setMessage("U wanna get this hands?");
+        face.setImage("enraged");
     }
     
-    public void sleep() {
-        hunger = hunger + 1;
+    public void work() {
+        excitement = excitement + 1;
+        face.setImage("verysick");
+    }
+
+    public void chill(){
+        excitement = excitement + 1;
+        face.setMessage("Its the best day ever!!!");
         face.setImage("asleep");
     }
+    public void football(){
+        excitement = excitement+1;
+        face.setMessage("Thats a goalllllll  Wooooahhhhhhhhhh!");
+        face.setImage("football");
+    }
+ 
+   
+    
+;
 
 } // end Virtual Pet
